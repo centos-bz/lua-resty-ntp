af8798048d910663c5d83872a9140289dd19f90f
centos-bz <admin@centos.bz>
2017-03-22 21:04:46 +0800

    add ntp library

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# lua-resty-ntp
+Name
+====
+
+lua-resty-ntp - Get utc time from ntp server for ngx_lua and LuaJIT
+
+Table of Contents
+=================
+
+* [Name](#name)
+* [Description](#description)
+* [Synopsis](#synopsis)
+* [Author](#author)
+* [See Also](#see-also)
+
+
+
+Description
+===========
+
+This library requires the [ngx_lua module](http://wiki.nginx.org/HttpLuaModule), and [LuaJIT 2.0](http://luajit.org/luajit.html).
+
+Synopsis
+========
+
+```lua
+    # nginx.conf:
+
+    lua_package_path "/path/to/lua-resty-string/lib/?.lua;;";
+
+    server {
+        location = /test {
+            content_by_lua_block {
+                local ntp = require "ntp"
+                -- ntp.utctime(ntp_server, retry , timeout)
+                local utctime,err =  ntp.utctime("pool.ntp.org", 3, 1000)
+                if utctime then
+                    ngx.say("utctime:",utctime)
+                else
+                    ngx.say("failed to get utctime: ",err)
+                end           
+            }
+        }
+    }
+
+```
+
+Author
+======
+
+Jason zhu (朱茂海) <admin@centos.bz>
+
+See Also
+========
+* the ngx_lua module: http://wiki.nginx.org/HttpLuaModule
---
+++ lib/resty/ntp.lua
@@ -0,0 +1,67 @@
+-- Copyright (C) Maohai Zhu (admin@centos.bz).
+
+local struct = require("struct")
+local ngx_socket_udp = ngx.socket.udp
+local table_concat = table.concat
+
+local function connect(ntp_server, timeout)
+    -- set timeout
+    local sock = ngx_socket_udp()
+    sock:settimeout(timeout)
+
+    -- connect ntp server
+    local ok, err = sock:setpeername(ntp_server, 123)
+    if not ok then
+        return nil, table_concat({"failed to connect to ntp: ", err})
+    end
+
+    -- pack and send data to ntp server
+    local data = struct.pack("<IIIIIIIIIIII",0x23,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0)
+    local ok, err = sock:send(data)
+    if not ok then
+        return nil, "failed to send."
+    end
+
+    -- receive data
+    local data, err = sock:receive()
+    if not data then    
+        return nil, table_concat({"failed to read a packet: ", err})
+    end
+    sock:close()
+
+    -- unpack data
+    local _,_,_,_,_,_,_,_,time = struct.unpack(">IIIIIIIII",data)
+    local utc_time = time - 2208988800
+    return utc_time
+
+end
+
+local function utctime(ntp_server, retry, timeout)
+    -- check the parameter
+    if not ntp_server then
+        return nil, "ntp_server is nil"
+    end
+    
+    if not retry then
+        return nil, "retry is nil"
+    end
+
+    if not timeout then
+        return nil, "timeout is nil"
+    end
+
+    local utc_time, err
+
+    for i=1,retry do
+        utc_time, err = connect(ntp_server, timeout)
+        if utc_time then
+            break
+        end    
+    end
+    return utc_time, err
+    
+end
+
+return {
+    utctime = utctime
+}
